b2333bc08168da462b3d43f09a4bc1181196270f
Ashish1692 <ashishmoghe6@gmail.com>
2023-11-25 15:08:14 +0530

    modifyed

--- Wage_Tracker_Form.py
+++ Wage_Tracker_Form.py
@@ -44,8 +44,8 @@ existing_data = existing_data.dropna(how="all")
 statusopt = ["Done", "Pending", "Cancelled"]
 st.markdown(hide_menu, unsafe_allow_html=True)
 with st.form(key="vendor_form"):
-    paidby = st.text_input(label="Paid By*",autocomplete=True,placeholder="Enter your name")
-    content = st.text_input(label="Content*",autocomplete=True,placeholder="Lunch,Dinner,Breakfast...")
+    paidby = st.text_input(label="Paid By*",placeholder="Enter your name")
+    content = st.text_input(label="Content*",placeholder="Lunch,Dinner,Breakfast...")
     amountpaid = st.number_input(label="Amount Paid*",min_value=1,max_value=100_000)
     friendcount = st.number_input(label="Friend Count*",min_value=1,max_value=25)
     description = st.text_area(label="Description*")
